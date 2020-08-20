47acdfb705a042cad8be3c083dda853ffe7a4cae
alyrtmrt <alayurtmm@gmail.com>
2020-08-20 15:37:51 -0400

    added sigin-and-sign-up component, added signin feature

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.js'
 import Header from './components/header/header.js'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.js'
 
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
      <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/shop" component={ShopPage} />
+      <Route path="/signin" component={SignInAndSignUpPage} />
+
      </Switch>
     </div>
   );
