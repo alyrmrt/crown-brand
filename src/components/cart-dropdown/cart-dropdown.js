8fb821fae6fe58dcd31d4b3d1589019db3ae6bc1
alyrtmrt <alayurtmm@gmail.com>
2020-08-26 09:25:57 -0400

    fixed bug on cart dropdown component

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -6,7 +6,7 @@ import './cart-dropdown.styles.scss'
 const CartDropdown = () => (
   <div className='cart-dropdown'>
    <div className='cart-items'>
-  <CustomButton>GO TO CHECOUT </CustomButton>
+    <CustomButton>GO TO CHECOUT </CustomButton>
    </div>
   </div>
 )
