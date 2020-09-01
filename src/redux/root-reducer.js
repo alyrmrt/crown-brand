f1b3c40e3f8af2012829bbfee8d8547fe9257fe7
alyrtmrt <alayurtmm@gmail.com>
2020-09-01 15:15:40 -0400

    transferred data flow to do redux via redux/shop

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,7 +3,8 @@ import { persistReducer } from 'redux-persist'
 import  storage  from 'redux-persist/lib/storage'
 import userReducer from './user/user.reducer.js'
 import cartReducer from './cart/cart.reducer.js'
-
+import directoryReducer from './directory/directory.reducer.js'
+import shopReducer from './shop/shop.reducer.js'
 
 const persistConfig = {
   key: 'root',
@@ -13,7 +14,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 })
 
 export default persistReducer(persistConfig, rootReducer)
