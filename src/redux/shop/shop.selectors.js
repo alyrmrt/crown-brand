ee7a0b27137cec2d75cb6b15efc1ef285afdf115
alyrtmrt <alayurtmm@gmail.com>
2020-09-02 10:05:49 -0400

    updated shop.data com to as an object

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,6 @@
 import { createSelector } from 'reselect'
+// import memoize from 'lodash.memoize'
 
-
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-}
 
 const selectShop = state => state.shop
 
@@ -19,8 +12,6 @@ export const selectCollections = createSelector(
 export const selectCollection = collectionUrlParam =>
 createSelector(
   [selectCollections],
-  collections =>
-    collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  collections => collections[collectionUrlParam]
+
 )
