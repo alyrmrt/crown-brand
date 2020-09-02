e6d871bac8c36307646d05b32ef948387326e0c5
alyrtmrt <alayurtmm@gmail.com>
2020-09-02 10:21:36 -0400

    fixed bug on collections.overview

--- src/components/collections-overview/collections-overview.js
+++ src/components/collections-overview/collections-overview.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CollectionPreview from '../collection-preview/collection-preview.js';
-import { selectCollections } from '../../redux/shop/shop.selectors.js'
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors.js'
 
 const CollectionsOverview = ({ collections }) => (
   <div className='collections-overview'>
@@ -13,7 +13,7 @@ const CollectionsOverview = ({ collections }) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
