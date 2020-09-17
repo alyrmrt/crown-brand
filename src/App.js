14288b22cf2e5765d9acf6946b49276c4366b7d1
alyrtmrt <alayurtmm@gmail.com>
2020-09-16 21:11:54 -0400

    deleted and rewrited backend code due to  fetching data problem from firebas to client

--- src/App.js
+++ src/App.js
@@ -1,72 +1,82 @@
-import React, { Component } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
-import CheckoutPage from './pages/checkout/checkout.js'
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.js'
-import Header from './components/header/header.js'
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.js'
-import { auth, createUserProfileDocument } from './firebase/firebase.utils.js'
-import { setCurrentUser } from './redux/user/actions.js'
-import { selectCurrentUser } from './redux/user/selector.js'
 
-class App extends Component  {
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 
-  unsubscribeFromAuth = null
+import Header from './components/header/header.component';
 
-  componentDidMount(){
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
-    const {setCurrentUser } = this.props
+import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
+
+class App extends React.Component {
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth)
+        const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-           setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-          })
-        })
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
       }
-      setCurrentUser(userAuth)
-    })
+
+      setCurrentUser(userAuth);
+    });
   }
 
   componentWillUnmount() {
-    this.unsubscribeFromAuth()
+    this.unsubscribeFromAuth();
   }
 
   render() {
     return (
       <div>
-      <Header />
-       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route exact path="/checkout" component={CheckoutPage} />
-        <Route  exact path="/signin" render={() =>
-          this.props.currentUser ? (
-            <Redirect to='/' />
-          ) : (
-            <SignInAndSignUpPage />
-          )
-        }
-      />
-       </Switch>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
+        </Switch>
       </div>
     );
   }
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-})
+  currentUser: selectCurrentUser
+});
 
 const mapDispatchToProps = dispatch => ({
-    setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,7 +1,6 @@
-import { UserActionTypes} from './types.js'
-
+import { UserActionTypes } from './user.types';
 
 export const setCurrentUser = user => ({
   type: UserActionTypes.SET_CURRENT_USER,
   payload: user
-})
+});
